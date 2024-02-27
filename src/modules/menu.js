e861a73ed0330a7f8a635dd43f5f248ba6a7aec8
Victor Saprykin <victanik@mail.ru>
2024-02-27 18:25:20 +0300

    chceck2 less 22

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,8 +1,8 @@
 const menu = () => {
   const menuBtn = document.querySelector('.menu');
   const menu = document.querySelector('menu');
-  const closeBtn = menu.querySelector('.close-btn');
-  const menuItems = menu.querySelectorAll('ul>li>a');
+  // const closeBtn = menu.querySelector('.close-btn');
+  // const menuItems = menu.querySelectorAll('ul>li>a');
 
   const handleMenu = () => {
     menu.classList.toggle('active-menu');
@@ -11,8 +11,10 @@ const menu = () => {
   menuBtn.addEventListener('click', handleMenu);
 
   menu.addEventListener('click', (e) => {
-    console.log(e.target.closest('menu'));
-    if (e.target.closest('menu') && !e.target.classList.contains('active-menu')) {
+    if (
+      e.target.closest('menu') &&
+      !e.target.classList.contains('active-menu')
+    ) {
       handleMenu();
     } else {
       return;
@@ -20,4 +22,3 @@ const menu = () => {
   });
 };
 export default menu;
-
